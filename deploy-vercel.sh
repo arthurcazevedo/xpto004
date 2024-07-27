40256876d22896506fdac05dc2cdeb13b4cae3c4
Arthur Silva de Carvalho Azevedo <arthur.c.azevedo@gmail.com>
2024-07-27 09:59:45 -0300

    mais um teste

--- deploy-vercel.sh
+++ deploy-vercel.sh
@@ -6,5 +6,9 @@ curl -fsSL https://deno.land/x/install/install.sh | sh
 # Install pagic
 /vercel/.deno/bin/deno install --unstable --allow-read --allow-write --allow-net https://deno.land/x/pagic/mod.ts
 
+# copiando o pagic.config.ts
+
+mkdir dist
+cp pagic.config.ts dist/
 # Pagic build
 /vercel/.deno/bin/deno run --unstable --allow-read --allow-write --allow-net --allow-env --allow-run https://deno.land/x/pagic/mod.ts build
